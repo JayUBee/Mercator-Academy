bddf0480f6e1a722bb4a7de338da4fbdaaff7e18
Tayyab Butt <tayyab.butt@mac.home>
2025-06-23 13:57:50 +0100

    Alerts

--- src/main/scala/Automation/Week7Day1/AlertsHandling.scala
+++ src/main/scala/Automation/Week7Day1/AlertsHandling.scala
@@ -1,5 +1,19 @@
 package Automation.Week7Day1
 
-object AlertsHandling {
+import org.openqa.selenium.{By, WebDriver}
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 
+object AlertsHandling extends App {
+
+  val options = new ChromeOptions
+  options.addArguments("--headless")
+
+  val driver: WebDriver = new ChromeDriver(options)
+
+  driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+
+  driver.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[1]/button")).click()
+
+
+  driver.quit()
 }
